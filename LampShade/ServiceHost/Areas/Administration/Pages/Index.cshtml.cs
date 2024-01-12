4f8d727653e38dec6ee13dd7b64e5fcf3feb4713
Mahziyar <mahziyar138006@gmail.com>
2024-01-12 04:50:55 -0800

    Add: serviceHost And Admin Assets

---
+++ LampShade/ServiceHost/Areas/Administration/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas.Administration.Pages;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
